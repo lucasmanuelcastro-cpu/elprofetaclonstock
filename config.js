1baa9dd4a260d41cf02fbbc79ded89322ce05ce0
lucasmanuelcastro-cpu <lucasmanuelcastro@gmail.com>
2026-07-10 12:43:55 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+/**
+ * CONFIG.JS - El Profeta
+ * ============================================================
+ * ÚNICO lugar donde tenés que pegar la URL de tu Google Apps
+ * Script una vez que lo despliegues (ver INSTALACION.md).
+ *
+ * Ejemplo:
+ * const URL_SCRIPT = "https://script.google.com/macros/s/AKfycb.../exec";
+ * ============================================================
+ */
+const URL_SCRIPT = "PEGA_ACA_TU_URL_DE_GOOGLE_APPS_SCRIPT";
